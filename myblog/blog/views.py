936ac2b45320ce5c469463bb79523c526fbd1992
Eduard Kaverinskyi <eduard.kaverinskyi@gmail.com>
2025-03-30 20:21:21 +0200

    refactor (blog): implement post_list view as class-based
    Signed-off-by: Eduard Kaverinskyi <eduard.kaverinskyi@gmail.com>

--- myblog/blog/views.py
+++ myblog/blog/views.py
@@ -1,18 +1,14 @@
-from django.core.paginator import Paginator
 from django.shortcuts import render, get_object_or_404
-from django.http import Http404
+from django.views.generic import ListView
 from .models import Post
 
 
 # Create your views here.
-def post_list(request):
-    post_list = Post.published.all()
-
-    paginator = Paginator(post_list, 3)
-    page_number = request.GET.get("page", 1)
-    posts = paginator.page(page_number)
-
-    return render(request, "blog/post/list.html", {"posts": posts})
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = "posts"
+    paginate_by = 3
+    template_name = "blog/post/list.html"
 
 
 def post_detail(request, year, month, day, post):
