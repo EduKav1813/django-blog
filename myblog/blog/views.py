9083633a6eb4dae48a5e90bab9994097d0bd4613
Eduard Kaverinskyi <eduard.kaverinskyi@gmail.com>
2025-04-05 14:05:40 +0200

    feat (blog): add post sharing via e-mail
    Signed-off-by: Eduard Kaverinskyi <eduard.kaverinskyi@gmail.com>

--- myblog/blog/views.py
+++ myblog/blog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView
 from .models import Post
+from .forms import EmailPostForm
+from django.core.mail import send_mail
 
 
 # Create your views here.
@@ -21,3 +23,45 @@ def post_detail(request, year, month, day, post):
         status=Post.Status.PUBLISHED,
     )
     return render(request, "blog/post/detail.html", {"post": post})
+
+
+def post_share(request, post_id):
+    post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+    sent = False
+
+    if request.method == "POST":
+        form = EmailPostForm(request.POST)
+
+        if form.is_valid():
+            cd = form.cleaned_data
+
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+            subject = (
+                f"{cd['name']} ({cd['email']}) "
+                f"Recommends you to read ({post.title})"
+            )
+            message = (
+                f'Read "{post.title}" at {post_url}\n\n'
+                f"{cd["name"]} comments:\n{cd["comments"]}"
+            )
+
+            send_mail(
+                subject=subject,
+                message=message,
+                from_email=None,
+                recipient_list=[cd["to"]],
+            )
+        sent = True
+
+    else:
+        form = EmailPostForm()
+
+    return render(
+        request,
+        "blog/post/share.html",
+        {
+            "post": post,
+            "form": form,
+            "sent": sent,
+        },
+    )
--- myblog/myblog/settings.py
+++ myblog/myblog/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,7 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "blog.apps.BlogConfig"
+    "blog.apps.BlogConfig",
 ]
 
 MIDDLEWARE = [
@@ -122,3 +123,11 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+EMAIL_HOST = "localhost"
+EMAIL_HOST_USER = config("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD")
+EMAIL_PORT = 1026
+EMAIL_USE_TLS = False
+EMAIL_USE_SSL = False
+DEFAULT_FROM_EMAIL = config("DEFAULT_FROM_EMAIL")
