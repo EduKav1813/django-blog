aaad3c0033afa31c9976507629b7f7aab8dd1b13
Eduard Kaverinskyi <eduard.kaverinskyi@gmail.com>
2025-04-06 14:02:28 +0200

    feat (blog): implement comment system
    Signed-off-by: Eduard Kaverinskyi <eduard.kaverinskyi@gmail.com>

--- myblog/blog/views.py
+++ myblog/blog/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView
+from django.views.decorators.http import require_POST
 from .models import Post
-from .forms import EmailPostForm
+from .forms import EmailPostForm, CommentForm
 from django.core.mail import send_mail
 
 
@@ -22,7 +23,13 @@ def post_detail(request, year, month, day, post):
         publish__day=day,
         status=Post.Status.PUBLISHED,
     )
-    return render(request, "blog/post/detail.html", {"post": post})
+    comments = post.comments.filter(active=True)
+    form = CommentForm()
+    return render(
+        request,
+        "blog/post/detail.html",
+        {"post": post, "comments": comments, "form": form},
+    )
 
 
 def post_share(request, post_id):
@@ -64,4 +71,22 @@ def post_share(request, post_id):
             "form": form,
             "sent": sent,
         },
+    )
+
+
+@require_POST
+def post_comment(request, post_id):
+    post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+    comment = None
+
+    form = CommentForm(data=request.POST)
+    if form.is_valid():
+        comment = form.save(commit=False)
+        comment.post = post
+        comment.save()
+
+    return render(
+        request,
+        "blog/post/comment.html",
+        {"post": post, "form": form, "comment": comment},
     )
